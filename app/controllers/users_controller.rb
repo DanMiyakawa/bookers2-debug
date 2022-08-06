208015cdc11bff085ff6646a318e5e66426c82a9
“DanMiyakawa” <110312863+DanMiyakawa@users.noreply.github.com>
2022-08-06 05:43:49 +0000

    9a途中

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "You have updated user successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(current_user) unless @user == current_user
+  end
+  
+
+end
